de24efb8e747411fb1ce47c635e7cdd8b8400fcc
Sigit Prayoga <prayoga.sigit@gmail.com>
2017-03-06 22:22:37 +0700

    Get, Add, Update

--- main.go
+++ main.go
@@ -11,16 +11,18 @@ import (
 var db *pg.DB
 
 func main() {
+
 	// Init DB connection
-	connectDb()
+	db = initDB()
 
 	// Register all end-points
 	http.HandleFunc("/", helloServer)
 	http.HandleFunc("/todos", getTodos)
-	http.HandleFunc("/todos/create", addTodo)
+	http.HandleFunc("/todos/add", addTodo)
+	http.HandleFunc("/todos/update", updateTodo)
 
-	// Start to listen
-	http.ListenAndServe(":8383", nil)
+	log.Println("Listening...")
+	log.Fatal(http.ListenAndServe(":8383", nil))
 }
 
 func helloServer(w http.ResponseWriter, req *http.Request) {
@@ -30,32 +32,19 @@ func helloServer(w http.ResponseWriter, req *http.Request) {
 
 func getTodos(w http.ResponseWriter, req *http.Request) {
 	// Init empty Todo
-	todos := []ToDo{}
+	todos := []*Todo{}
 
 	// Do 'select' into DB
 	err := db.Model(&todos).Select()
 	if err != nil {
 		log.Fatal("Error in Select", err)
 	}
-	// Send response back
-	data, err := json.Marshal(todos)
-	if err != nil {
-		log.Fatal("Not making any sense", err)
-	}
-	w.Write(data)
+
+	writeResponse(w, &todos, http.StatusOK)
 }
 
 func addTodo(w http.ResponseWriter, req *http.Request) {
-	// Init new decoder from the Body
-	decoder := json.NewDecoder(req.Body)
-	var newTodo ToDo
-	decodeErr := decoder.Decode(&newTodo)
-	if decodeErr != nil {
-		log.Fatal("SHOOT! ", decodeErr)
-	}
-	// Close eventually
-	defer req.Body.Close()
-	log.Println(newTodo.Label, newTodo.Description)
+	newTodo := getTodoFromRequest(req)
 
 	// Insert into DB
 	insertErr := db.Insert(&newTodo)
@@ -63,25 +52,58 @@ func addTodo(w http.ResponseWriter, req *http.Request) {
 		log.Fatal("WTF ?!", insertErr)
 	}
 
-	// Send response back
-	data, err := json.Marshal(newTodo)
-	if err != nil {
-		log.Fatal("Not making any sense", err)
-	}
-	w.Write(data)
+	writeResponse(w, &newTodo, http.StatusOK)
 }
 
-func connectDb() {
+func updateTodo(w http.ResponseWriter, req *http.Request) {
+	todo := getTodoFromRequest(req)
+
+	// only 'done' column will get updated based on 'id'
+	_, err := db.Model(&todo).Column("done").Update()
+	if err != nil {
+		log.Fatal("Update FAILED!", err)
+	}
+
+	writeResponse(w, &todo, http.StatusOK)
+}
+
+func initDB() *pg.DB {
 	// same as 'psql -U postgres -d postgres' in terminal, connect to localhost:5432
-	db = pg.Connect(&pg.Options{
+	db := pg.Connect(&pg.Options{
 		User:     "postgres",
 		Database: "postgres",
 	})
+	return db
 }
 
-// ToDo Model = 'todos' table
-type ToDo struct {
-	tableName   struct{} `sql:"todos"`
-	Label       string
-	Description string
+func getTodoFromRequest(r *http.Request) Todo {
+	// Init new decoder from the Body
+	decoder := json.NewDecoder(r.Body)
+	var newTodo Todo
+	decodeErr := decoder.Decode(&newTodo)
+	if decodeErr != nil {
+		log.Fatal("SHOOT! ", decodeErr)
+	}
+	// Close eventually
+	defer r.Body.Close()
+
+	return newTodo
+}
+
+func writeResponse(w http.ResponseWriter, v interface{}, code int) {
+	data, err := json.Marshal(v)
+	if err != nil {
+		log.Printf("Marshalling error: %v", err)
+	}
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(code)
+	w.Write(data)
+}
+
+// Todo Model = 'todos' table
+type Todo struct {
+	tableName struct{} `sql:"todos"`
+	Todo      *string  `json:"todo"`
+	Done      bool     `json:"done"`
+	ID        string   `json:"id"`
 }
